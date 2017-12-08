5793d4e57d27d17baf5988ea10a3b7f2e0aba809
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-12-08 19:48:03 +0900

    table (#21)

--- base64.js
+++ base64.js
@@ -1,17 +1,34 @@
 'use strict';
 
 var base64 = (function() {
-  var ENCODE_BASE64_TABLE = [];
-  var DECODE_BASE64_TABLE = [];
-  (function(characters) {
-    var index, code;
-    for (index = characters.length - 1; index >= 0; --index) {
-      code = characters.charCodeAt(index);
-      ENCODE_BASE64_TABLE[index] = code & 0xff;
-      DECODE_BASE64_TABLE[code] = index;
-    }
-    DECODE_BASE64_TABLE['='.charCodeAt(0)] = 0;
-  })('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/');
+  var ENCODE_BASE64_TABLE = [
+     65,  66,  67,  68,  69,  70,  71,  72,
+     73,  74,  75,  76,  77,  78,  79,  80,
+     81,  82,  83,  84,  85,  86,  87,  88,
+     89,  90,  97,  98,  99, 100, 101, 102,
+    103, 104, 105, 106, 107, 108, 109, 110,
+    111, 112, 113, 114, 115, 116, 117, 118,
+    119, 120, 121, 122,  48,  49,  50,  51,
+     52,  53,  54,  55,  56,  57,  43,  47
+  ];
+  var DECODE_BASE64_TABLE = [
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0, 62,  0,  0,  0, 63,
+    52, 53, 54, 55, 56, 57, 58, 59,
+    60, 61,  0,  0,  0,  0,  0,  0,
+     0,  0,  1,  2,  3,  4,  5,  6,
+     7,  8,  9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21, 22,
+    23, 24, 25,  0,  0,  0,  0,  0,
+     0, 26, 27, 28, 29, 30, 31, 32,
+    33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48,
+    49, 50, 51,  0,  0,  0,  0,  0
+  ];
 
   function encode(source) {
     var sourceLength = source.length;
